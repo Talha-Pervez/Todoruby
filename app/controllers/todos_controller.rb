05a917d87156ef87b465f8cdd6f3a247e451d320
Talha-Pervez <talhapervez2000@gmail.com>
2023-07-31 11:56:36 +0500

    first commit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,6 +2,9 @@ class TodosController<ApplicationController
 	def new
 		@todo = Todo.new
 	end
+
+
+
 	def create
 		@todo = Todo.new(todo_params)
 		if @todo.save
@@ -11,13 +14,51 @@ class TodosController<ApplicationController
 			render 'new'
 		end
 	end
+
+
+
+
 	def show
 		@todo=Todo.find(params[:id])
 	end
 
-	private 
-	def todo_params
-		params.require(:todo).permit(:name,:description)
+
+
+	def edit
+		@todo=Todo.find(params[:id])
 	end
+
+	def destroy
+		@todo=Todo.find(params[:id])
+		@todo.destroy
+		flash[:notice]= "Todo was successfuly deleted"
+		redirect_to todos_path
+		
+	end
+
+	def update
+		@todo=Todo.find(params[:id])
+		if @todo.update(todo_params)
+			flash[:notice]= "Todo was successfuly updated"
+			redirect_to todo_path(@todo)
+		else 
+			render 'edit'
+		end
+	end
+
+
+
+	def index
+
+	 @todos=Todo.all
+	end
+
+
+
+
+	private 
+		def todo_params
+			params.require(:todo).permit(:name,:description)
+		end
 
 end
